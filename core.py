89d156629a189e680eca81ebf66831644aa3ab61
Adam Green <adam.green@adgefficiency.com>
2017-04-06 15:21:57 +0200

    updated outputs

--- core.py
+++ core.py
@@ -3,13 +3,14 @@ import pandas as pd
 import time
 import environments
 import agents
+import outputs
 
-EPISODES = 30
+EPISODES = 75
 EPISODE_LENGTH = 336  # number of half hour periods per episode
-GEN_OUTPUTS = 5  # generate outputs every n episodes
+GEN_OUTPUTS = 25  # generate outputs every n episodes
 
 env = environments.energy_py(EPISODE_LENGTH)
-agent = agents.Q_learner(env, verbose=0)
+agent = agents.Q_learner(env, verbose=0, device=0)
 episodes = np.linspace(1, EPISODES, EPISODES, endpoint=True).astype(int)
 
 print('Started at ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
@@ -21,14 +22,14 @@ for episode in episodes:
     agent.policy_ = 1  # e-greedy policy
     agent.single_episode(episode)
     if episode != 1 and episode % GEN_OUTPUTS == 0:
-        memory, network_memory, summary, states, actions = agent.outputs()
+        memory, network_memory, summary, states, actions = outputs.gen(agent)
 
 print('Running greedy episode')
 episode += 1
 agent.epsilon = 0  # greedy
 agent.single_episode(episode)
 agent.save_csv = True
-memory, network_memory, summary, states, actions = agent.outputs()
+memory, network_memory, summary, states, actions = outputs.gen(agent)
 agent.network.save('results/weights.h5')
 
 final_results = pd.DataFrame(
