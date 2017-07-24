7c3215c67900ec63d5d1442017c43a87938564cd
Adam Green <adam.green@adgefficiency.com>
2017-07-24 02:17:20 +0100

    setup.py changes

--- setup.py
+++ setup.py
@@ -1,16 +1,28 @@
 from setuptools import setup
 
+"""
+The setup.py script for energy_py
+"""
+
 setup(name='energy_py',
+
       version='2.0',
+
       description='Reinforcement learning for energy systems',
+
       author='Adam Green',
+
       author_email='adam.green@adgefficiency.com',
+
       url='http://adgefficiency.com/',
+
       packages=['energy_py',
                 'energy_py.agents',
                 'energy_py.envs',
                 'energy_py.main'],
+
       install_requires=['numpy',
                         'pandas',
                         'tensorflow-gpu',
-                        'keras'])
+                        'keras']
+      )
