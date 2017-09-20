f68312ef2ba522ae95100643d33ef53573c73bf7
Adam Green <adam.green@adgefficency.com>
2017-09-20 19:35:29 +0100

    printing formatting

--- energy_py/agents/policy_based/reinforce.py
+++ energy_py/agents/policy_based/reinforce.py
@@ -179,6 +179,6 @@ class REINFORCE_Agent(Base_Agent):
         _, loss = session.run([self.train_step, self.loss], feed_dict)
         self.memory.losses.append(loss)
 
-        print('loss is {} - mean discounted returns were {}'.format(float(loss), np.mean(discounted_returns)))
-        
+        print('loss is {:.8f} - mean discounted returns were {:.4f}'.format(float(loss), np.mean(discounted_returns)))
+
         return loss
