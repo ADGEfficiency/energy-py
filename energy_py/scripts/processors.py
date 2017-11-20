66e06f97af811bf4ac6b5bb080e67b7dbc038bfb
adam <adam.green@adgefficiency.com>
2017-11-20 21:20:45 +0000

    setup changes and processor work

--- energy_py/scripts/processors.py
+++ energy_py/scripts/processors.py
@@ -2,62 +2,61 @@
 Purpose of this script is to hold Processor objects - a class of objects
 used for functionality such as standardization or normalization.
 
-The idea behind using a class is that we can allow the class to collect
-statistics on the data it has seen so far - for example to estimate the mean
-for standardization from all data the Processor has processed so far.
+We can allow the class to collect statistics on the data it has seen so far.
+
+We can also choose to process a 
 """
 
 import numpy as np
 
-class SimpleStandardizer(object):
-    def __init__(self):
-        pass
 
-    def transform(self, data):
+class Standardizer(object):
+    """
+    We rely on the input shape being (n_samples, state_dim) 
+    """
+    def __init__(self):
+        #  use a list to hold all data this processor has seen
+        self.history = []
+
+    def transform(self, batch, use_history=True):
+        """
+        The SimpleStand:ardizer transforms the data using the mean and standard
+        deviation across the batch it is processing
+        """
+        #  check that our data is 
+        assert len(batch.shape) == 2
+        #  add the data we are processing onto our history list
+        self.history.append(batch)
+        if use_history:
+            #  create an array from the list then reshape to (num_samples, dim)
+            #  taking advantage of energy_py states/actions being this shape (always len 2)
+            history = np.array(self.history).reshape(-1, batch.shape[1])
+            means, stdevs = history.mean(axis=0), history.std(axis=0)
+        else:
+            means, stdevs = batch.mean(axis=0), batch.std(axis=0)
+        
+        return (batch - means) / stdevs
+
+
+class Normalizer(object):
+    """
+    We rely on the input shape being (n_samples, state_dim) 
+    """
+    def __init__(self):
+        #  use a list to hold all data this processor has seen
+        self.history = []
+
+    def transform(self, batch):
         """
         The SimpleStandardizer transforms the data using the mean and standard
         deviation across the batch it is processing
         """
-        return (data - data.mean()) / data.std()
+        assert len(batch.shape) == 2
+        #  add the data we are processing onto our history list
+        self.history.append(batch)
+        #  create an array from the list then reshape to (num_samples, dim)
+        #  taking advantage of energy_py states/actions being this shape (always len 2)
+        history = np.array(self.history).reshape(-1, batch.shape[1])
+        
+        return (batch - history.mean(axis=0)) / history.std(axis=0)
 
-class Standardizer(object):
-    """
-    Holds all historical data
-    """
-    def __init__(self):
-        self.data = np.array([])
-
-    def transform(self, data):
-        """
-        Transform the batch using the avergage mean & std we have seen over
-        the lifetime of the Standardizer
-        """
-        self.data = np.append(self.data, data)
-        means = np.mean(self.data, axis=1)
-        std = np.std(self.data, axis=1)
-        return (data - mean) / std
-
-class SimpleNormalizer(object):
-    def __init__(self):
-        pass
-
-    def transform(self, data):
-        """
-        Transforms the batch using the largest & smallest
-        """
-        maximum = np.maximum(data)
-        minimum = np.minimum(data)
-        return (data  - minimum) / (maximum - minimum)
-
-class Normalizer(object):
-    def __init__(self):
-        self.data = np.array([])
-
-    def transform(self, data):
-        """
-        Transforms the batch using the largest & smallest
-        """
-        self.data = np.append(self.data, data)
-        maximum = np.maximum(self.data, axis=1)
-        minimum = np.minimum(self.data, axis=1)
-        return (data  - minimum) / (maximum - minimum)
