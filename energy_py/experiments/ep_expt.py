a87cacb399f8d0101448376f020e733f1db5d18c
Adam Green <adam.green@adgefficiency.com>
2018-03-29 03:37:37 +0000

    setup first round of battery DQN expts

--- energy_py/experiments/ep_expt.py
+++ energy_py/experiments/ep_expt.py
@@ -5,27 +5,27 @@ from energy_py.agents import DQN, DPG
 from energy_py.envs import FlexEnv, BatteryEnv
 
 if __name__ == '__main__':
-    total_steps = 5e6
-    agent_config = {'discount': 0.97,
+    total_steps = 1e6
+    agent_config = {'discount': 0.99,
                     'tau': 0.001,
                     'total_steps': total_steps,
                     'batch_size': 32,
                     'layers': (25, 25, 25),
                     'learning_rate': 0.0001,
-                    'epsilon_decay_fraction': 0.3,
+                    'epsilon_decay_fraction': 0.4,
                     'memory_fraction': 0.15,
-                    'memory_type': 'priority',
-                    'double_q': True,
+                    'memory_type': 'deque',
+                    'double_q': False,
                     'process_observation': 'standardizer',
                     'process_target': 'normalizer'}
 
-    env = FlexEnv
+    env = BatteryEnv
     env_config = {'episode_length': 2016,
-                 # 'initial_charge': 'random',
+                  'initial_charge': 'random',
                   'episode_random': True}
 
     data_path = os.getcwd()+'/datasets/perfect_forecast/'
-    results_path = os.getcwd()+'/results/flex/'
+    results_path = os.getcwd()+'/results/battery_tests/'
 
     agent, env, sess = experiment(agent=DQN,
                                   agent_config=agent_config,
@@ -34,4 +34,5 @@ if __name__ == '__main__':
                                   total_steps=total_steps,
                                   data_path=data_path,
                                   results_path=results_path,
-                                  run_name='DDQN_priority')
+				  seed=42,
+                                  run_name='DQN_1')
