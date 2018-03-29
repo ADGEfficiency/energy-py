7701af4d7f840ad2baac45fca7784b426dd9d613
Adam Green <adam.green@adgefficiency.com>
2018-03-29 11:16:06 +0000

    aws expts

--- energy_py/experiments/ep_expt.py
+++ energy_py/experiments/ep_expt.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
                                   total_steps=total_steps,
                                   data_path=data_path,
                                   results_path=results_path,
-				  seed=42,
-                                  run_name='DQN_1')
+				  seed=10,
+                                  run_name='DQN_2')
