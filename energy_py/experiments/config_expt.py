d15969f4a05ca8645ef406b031c7258e97935b9a
Adam Green <adam.green@adgefficiency.com>
2018-04-06 11:29:39 +0930

    added example for the config experiment

--- energy_py/experiments/config_expt.py
+++ energy_py/experiments/config_expt.py
@@ -1,5 +1,8 @@
 """
 test - can test by reading the log file!
+
+python config_expt.py example example --run_name DQN_1
+
 """
 import os
 
@@ -7,14 +10,11 @@ from energy_py import make_expt_parser, run_config_expt
 
 
 if __name__ == '__main__':
-
-    #  entered in from the command line
-    expt_name = 'config_test'
-    run_name = 'DQN_1'
+    args = make_expt_parser()
 
     #  expt path is set here to get the current working directory
     expt_path = os.path.join(os.getcwd(),
                              'results',
-                             expt_name)
+                             args.expt_name)
 
-    results = run_config_expt(expt_name, run_name, expt_path)
+    run_config_expt(args.expt_name, args.run_name, expt_path)
