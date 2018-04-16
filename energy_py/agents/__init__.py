0edff9e571bb05c7cbe883a6604c8dc65962b322
Adam Green <adam.green@adgefficiency.com>
2018-04-16 08:45:48 +0100

    agent init cleanup

--- energy_py/agents/__init__.py
+++ energy_py/agents/__init__.py
@@ -1,5 +1,5 @@
-from energy_py.agents.memory import Memory, ArrayMemory, DequeMemory
 from energy_py.agents.memory import Experience, calculate_returns
+from energy_py.agents.memory import ArrayMemory, DequeMemory
 from energy_py.agents.priority_memory import PrioritizedReplay
 
 #  memories goes here because it's needed by BaseAgent
@@ -10,5 +10,5 @@ memories = {'array': ArrayMemory,
 from energy_py.agents.agent import BaseAgent
 from energy_py.agents.register import make_agent
 
-from energy_py.agents.classifier_agent import ClassifierCondition, ClassifierStragety
+from energy_py.agents.classifier_agent import ClassifierCondition
 from energy_py.agents.classifier_agent import ClassifierStragety
