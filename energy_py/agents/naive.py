76568b71ed88baee213e3678fdec7962ce6931ef
Adam Green <adam.green@adgefficiency.com>
2018-04-16 08:44:40 +0100

    agent folder refactor

--- energy_py/agents/naive.py
+++ energy_py/agents/naive.py
@@ -130,3 +130,39 @@ class NaiveFlex(BaseAgent):
             action = self.action_space.low 
 
         return np.array(action).reshape(1, self.action_space.shape[0])
+
+
+class RandomAgent(BaseAgent):
+    """
+    An agent that samples the action space.
+
+    Sample probability is used to control a balance between sampling and
+    just doing nothing.
+
+    args
+        env (object) energy_py environment
+        discount (float) discount rate
+        sample_probability (float) sampling action space and doing nothing
+    """
+    def __init__(self,
+                 env,
+                 discount,
+                 sample_probability=1.0):
+
+        self.sample_probability = float(sample_probability)
+        super().__init__(env, discount)
+
+    def _act(self, **kwargs):
+        """
+        Agent selects action randomly
+
+        returns
+             action (np.array)
+        """
+        if np.random.rand() <= self.sample_probability:
+            action = self.action_space.sample()
+        else:
+            action = self.action_space.low
+
+        return action.reshape(1, self.action_space.shape[0])
+
