170be9580bd5505a9ec64232010c8f8a0520cf91
Adam Green <adam.green@adgefficiency.com>
2018-06-12 21:37:18 +0100

    documenting tf utils and updating env readme

--- energy_py/common/tf_utils.py
+++ energy_py/common/tf_utils.py
@@ -1,28 +1,50 @@
+"""Helper functions for tensorflow"""
+
 import tensorflow as tf
 
 
 def get_tf_params(scope):
+    """
+    Makes a list of all tf.Variables under this scope
+
+    args
+        scope (str)
+
+    returns
+        params (list)
+    """
+    #  find scope parameters
     params = [p for p in tf.trainable_variables()
               if p.name.startswith(scope)]
 
     #  sort parameters list by the variable name
-    params = sorted(params, key=lambda var: var.name)
-
-    return params
+    return sorted(params, key=lambda var: var.name)
 
 
 def make_copy_ops(parent, child, scope='copy_ops'):
-    copy_ops = []
+    """
+    Creates the operations to copy variables 
 
+    args
+        parent (list of tf.Variables)
+        child (list of tf.Variables)
+
+    returns
+        copy_ops (list of tf.Operations)
+        tau (tf.placeholder)
+    """
     with tf.variable_scope(scope):
         tau = tf.Variable(1.0, name='tau')
 
+        copy_ops = []
         for p, c in zip(parent, child):
             assert p.name.split('/')[1:] == c.name.split('/')[1:]
 
             new_value = tf.add(tf.multiply(p, tau),
                                tf.multiply(c, 1 - tau))
+
             op = c.assign(new_value)
+
             copy_ops.append(op)
 
     return copy_ops, tau
--- energy_py/envs/readme.md
+++ energy_py/envs/readme.md
@@ -1,6 +1,10 @@
 # environments
 
-All energy_py environments are time series problems
+energy_py provides environments that energy time series problems, focusing on battery storage and flexibility.  Also provided are wrappers around Open AI gym environments such as cartpole and mountain-car.
+
+Environments are accessed via a [register](https://github.com/ADGEfficiency/energy_py/blob/master/energy_py/envs/register.py)
+
+## energy_py energy environments
 
 Data for the time series is loaded from a `state.csv` and `observation.csv` 
 
@@ -9,3 +13,8 @@ These csvs are found in the relevant dataset folder
 The example dataset is in `energy_py/experiments/datasets/example`
 
 All of energy_py works on a **5 minute basis**  
+
+## Open AI gym environments
+Custom built wrappers are made around gym environments to allow use with energy_py agents via the same API as for energy_py envs
+
+gym environments are included because they allow benchmarking of agents on well built and formulated environments
