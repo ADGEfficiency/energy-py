ac26ea79e5dfacc0ef48f7a985f9c280a1ccdbcd
Adam Green <adam.green@adgefficiency.com>
2018-09-08 00:32:12 +0200

    setup.py now installs energy_py/datasets/example/*.csv - so the example dataset will be included as package data.  other datasets will be loaded from a user supplied absolute path

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 from setuptools import setup, find_packages
 
-"""
-The setup.py script for energy_py
-"""
 
-setup(name='energy_py',
+setup(
+    name='energy_py',
 
-      version='0.2',
+    version='0.2',
+    description='reinforcement learning for energy systems',
+    author='Adam Green',
+    author_email='adam.green@adgefficiency.com',
+    url='http://adgefficiency.com/',
 
-      description='reinforcement learning for energy systems',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    package_dir={'energy_py': 'energy_py'},
+    package_data={'energy_py': ['experiments/datasets/example/*.csv']},
 
-      author='Adam Green',
-
-      author_email='adam.green@adgefficiency.com',
-
-      url='http://adgefficiency.com/',
-
-      packages=find_packages(exclude=['tests', 'tests.*']),
-
-      package_data = {'':['*.csv']},
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      install_requires=[]
-      )
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    install_requires=[]
+)
