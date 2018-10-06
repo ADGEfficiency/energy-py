09ea1dc98d420e34c979a2270dbb6cf22f5dada1
Adam Green <adam.green@adgefficiency.com>
2018-10-06 17:32:47 +1000

    format of load dataset

--- energypy/experiments/load_dataset.py
+++ energypy/experiments/load_dataset.py
@@ -7,21 +7,17 @@ import pandas as pd
 
 def load_dataset(dataset, name):
     """ load example dataset or load from user supplied path """
-
     if dataset == 'example':
-
-        path = 'experiments/datasets/example/{}.csv'.format(name) 
+        path = 'experiments/datasets/example/{}.csv'.format(name)
         data = pkg_resources.resource_string('energypy', path)
 
-        csv = pd.read_csv(
+        return pd.read_csv(
             io.BytesIO(data), index_col=0, parse_dates=True
         )
 
     else:
-        csv = pd.read_csv(
+        return pd.read_csv(
             join(dataset, name + '.csv'),
             index_col=0,
             parse_dates=True
         )
-
-    return csv
