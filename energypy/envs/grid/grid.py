72730694406dee6bc3a002ea8803673a2c7fc904
Adam Green <adam.green@adgefficiency.com>
2018-10-23 02:50:28 +1300

    working on electric grid based on market - dispatch working

--- energypy/envs/grid/grid.py
+++ energypy/envs/grid/grid.py
@@ -3,25 +3,32 @@ from collections import namedtuple
 import heapq as h
 
 import numpy as np
+import pandas as pd
 
 
-Bid = namedtuple('bid', ['price', 'offer', 'dispatch', 'name'])
+Bid = namedtuple('bid', ['price', 'offer', 'dispatch', 'date', 'name'])
+Bid.__new__.__defaults__ = (None,) * len(Bid._fields)
 
 
-def settle_market(market, demand):
+def settle_market(bid_stack, demand, date):
     """ finds the cheapest bids to match demand """
-    h.heapify(market)
+    h.heapify(bid_stack)
 
     dispatch, bids = 0, []
     while dispatch < demand:
-        next_cheapest = h.heappop(market)
+        try:
+            next_cheapest = h.heappop(bid_stack)
+        except IndexError:
+            raise ValueError(
+                'offers are less than demand of {} MW'.format(demand))
         bid_dispatch = np.min([demand - dispatch, next_cheapest.offer])
 
         bids.append(
-            Bid(next_cheapest.price,
-                next_cheapest.offer,
-                bid_dispatch,
-                next_cheapest.name)
+            Bid(price=next_cheapest.price,
+                offer=next_cheapest.offer,
+                dispatch=bid_dispatch,
+                name=next_cheapest.name,
+                date=date)
         )
 
         dispatch += bid_dispatch
@@ -32,11 +39,11 @@ def settle_market(market, demand):
 def test_grid():
     """ settle a simple two bid market """
     market = [
-        Bid(50, 100, None, 'coal'),
-        Bid(10, 10, None, 'wind')
+        Bid(price=50, offer=100, name='coal'),
+        Bid(price=10, offer=10, name='wind')
     ]
 
-    bids = settle_market(market, demand=50)
+    bids = settle_market(market, demand=50, date='test')
 
     assert bids[0].dispatch == 10
     assert bids[0].name == 'wind'
@@ -45,3 +52,42 @@ def test_grid():
 
 
 test_grid()
+
+
+class Participant():
+
+    def __init__(
+            self,
+            name,
+            prices=(0, 100),
+            offers=(0, 50)
+    ):
+        self.name = name
+        self.prices = prices
+        self.offers = offers
+
+    def bid(self, observation=None):
+
+        #  random price and offer
+        return Bid(
+            price=np.random.randint(low=self.prices[0], high=self.prices[1]),
+            offer=np.random.randint(low=self.offers[0], high=self.offers[1]),
+            name=self.name
+        )
+
+market = [
+    Participant('wind', prices=(0, 10)),
+    Participant('coal', prices=(50, 51))
+]
+
+bid_stack = [participant.bid() for participant in market]
+
+dispatch = settle_market(bid_stack, demand=25, date='1')
+
+print(dispatch)
+
+#  could do this in settlemarket
+df = pd.concat(
+    [pd.DataFrame(bid._asdict(), index=[bid.date]) for bid in dispatch],
+    axis=0
+)
--- energypy/envs/grid/readme.md
+++ energypy/envs/grid/readme.md
@@ -1,3 +1,6 @@
 #  Electric grid with market dispatch
 
+- market where demand is met using lowest cost generation first
+- all dispatched generators get the same price for their generation
+
 
