a6a0bf571339f0b79b205f7e699d0c53772a7a7b
Adam Green <adam.green@adgefficiency.com>
2025-03-16 02:17:32 +1300

    chore: new-world

--- energypy/__init__.py
+++
@@ -1,3 +0,0 @@
-from energypy.agent import alpha, memory, policy, random_policy, qfunc, target
-from energypy.registry import make
-from energypy.sampling import episode
--- tests/test_checkpoint.py
+++
@@ -1,8 +0,0 @@
-from energypy import checkpoint
-
-
-if __name__ == '__main__':
-
-    #  run?  or use a mock?
-
-    checkpoint = checkpoint.load(checkpoint)
