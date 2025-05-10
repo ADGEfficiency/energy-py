d45adfa3ce744bc167d721f3a7062d14df3beffe
Adam Green <adam.green@adgefficiency.com>
2025-05-10 16:34:23 +1200

    feat

--- examples/battery_arbitrage_experiments.py
+++ examples/battery_arbitrage_experiments.py
@@ -1,4 +1,4 @@
-import pathlib
+import collections
 import uuid
 
 import numpy as np
@@ -8,36 +8,47 @@ from stable_baselines3 import PPO
 import energypy
 from energypy.dataset import load_electricity_prices
 
-data = load_electricity_prices(
-    data_dir=pathlib.Path("data"), download_if_missing=True, verbose=True
+data = load_electricity_prices()
+
+n_lags = 0
+n_horizons = 12
+
+data = data.with_columns(
+    [pl.col("price").shift(n).alias(f"lag-{n}") for n in range(n_lags, n_lags + 1)]
 )
-prices = data["price"]
-features = prices.clone().to_frame()
-features = features.with_columns(
-    [pl.col("price").shift(n).alias(f"lag-{n}") for n in range(12)]
+data = data.with_columns(
+    [
+        pl.col("price").shift(-1 * n).alias(f"horizon-{n}")
+        for n in range(1, n_horizons + 1)
+    ]
 )
-features = features.drop_nulls()
+data = data.drop_nulls()
 
-limit_idx = min(data.shape[0], 6 * 30 * 48)
-data = data.slice(0, limit_idx)
-prices = prices.slice(0, limit_idx)
-features = features.slice(0, limit_idx)
+prices = data["price"].to_numpy()
+features = data.select(
+    pl.selectors.starts_with("horizon-"), pl.selectors.starts_with("lag-")
+).to_numpy()
 
-split_idx = int(data.shape[0] // 2)
-prices_tr = prices.slice(0, split_idx)
-prices_te = prices.slice(split_idx, data.shape[0])
+te_tr_split_idx = int(data.shape[0] * 0.8)
 
-features_tr = features.slice(0, split_idx)
-features_te = features.slice(split_idx, data.shape[0])
+prices_tr = prices[0:te_tr_split_idx]
+features_tr = features[0:te_tr_split_idx]
+
+prices_te = prices[te_tr_split_idx:]
+features_te = features[te_tr_split_idx:]
+
+print(f"prices_tr: {prices_tr.shape} features_tr: {features_tr.shape}")
+print(f"prices_te: {prices_te.shape} features_te: {features_te.shape}")
 
 expt_guid = uuid.uuid4()
 configs = []
-for noise in [0, 10, 1000]:
+noise = [0.0, 0.1, 0.5, 0.75, 1, 5, 25, 100, 1000]
+for noise_var in noise:
     run_guid = uuid.uuid4()
-    env_tr = energypy.make_env(electricity_prices=prices_tr, features=features)
+    env_tr = energypy.make_env(electricity_prices=prices_tr, features=features_tr)
     env_te = energypy.make_env(
         electricity_prices=prices_te,
-        features=prices_te * np.random.normal(0, noise, size=prices_te.shape[0]),
+        features=features_te * np.random.normal(0, noise_var, size=features_te.shape),
     )
 
     config = energypy.ExperimentConfig(
@@ -57,9 +68,9 @@ for noise in [0, 10, 1000]:
             verbose=1,
             tensorboard_log=f"./data/tensorboard/battery_arbitrage_experiments/{expt_guid}/run/{run_guid}",
         ),
-        name=f"battery_noise_{noise}",
-        n_learning_steps=2000,
-        n_eval_episodes=10,
+        name=f"battery_noise_{noise_var}",
+        n_learning_steps=5000,
+        n_eval_episodes=30,
     )
     configs.append(config)
 
@@ -67,16 +78,10 @@ results = energypy.run_experiments(
     configs, log_dir=f"./data/tensorboard/battery_arbitrage_experiments/{expt_guid}"
 )
 
-best_idx = np.argmax([r.checkpoints[-1].mean_reward_te for r in results])
-best_config = configs[best_idx]
-best_result = results[best_idx].checkpoints[-1]
+expt = collections.defaultdict(list)
+for noise_var, result in zip(noise, results):
+    cp = result.checkpoints[-1]
+    expt["noise_var"].append(noise_var)
+    expt["mean_reward_te"].append(cp.mean_reward_te)
 
-print(f"Best configuration: {best_config.name}")
-print(f"Learning rate: {best_config.agent.learning_rate}")
-print(f"Gamma: {best_config.agent.gamma}")
-print(
-    f"Test reward: {best_result.mean_reward_te:.2f} ± {best_result.std_reward_te:.2f}"
-)
-print(
-    f"Train reward: {best_result.mean_reward_tr:.2f} ± {best_result.std_reward_tr:.2f}"
-)
+print(pl.DataFrame(expt))
